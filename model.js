9e5234261ae7788b5e1ff495687801fc418121e1
Dmitry Mihaylov <dmacompton@gmail.com>
2015-10-31 21:58:08 +0200

    first commit

---
+++ model.js
@@ -0,0 +1,79 @@
+﻿var model = [
+	{
+		title: "Вопрос 1ый",
+		answers: [
+			{
+				title: 'Ответ 1',
+				rate: '1'
+			},
+			{
+				title: 'Ответ 2',
+				rate: '2'
+			},
+			{
+				title: 'Ответ 3',
+				rate: '3'
+			},
+			{
+				title: 'Ответ 4',
+				rate: '4'
+			},
+			{
+				title: 'Ответ 5',
+				rate: '5'
+			}
+		]
+	},
+	{
+		title: "Вопрос 2ой",
+		answers: [
+			{
+				title: 'Ответ 1',
+				rate: '1'
+			},
+			{
+				title: 'Ответ 2',
+				rate: '2'
+			},
+			{
+				title: 'Ответ 3',
+				rate: '3'
+			},
+			{
+				title: 'Ответ 4',
+				rate: '4'
+			},
+			{
+				title: 'Ответ 5',
+				rate: '5'
+			}
+		]
+	},
+	{
+		title: "Вопрос 3ий",
+		answers: [
+			{
+				title: 'Ответ 1',
+				rate: '1'
+			},
+			{
+				title: 'Ответ 2',
+				rate: '2'
+			},
+			{
+				title: 'Ответ 3',
+				rate: '3'
+			},
+			{
+				title: 'Ответ 4',
+				rate: '4'
+			},
+			{
+				title: 'Ответ 5',
+				rate: '5'
+			}
+		]
+	}
+];
+
+
